ab4344d65410b1679cd059b20767510e75d98846
kerami <keramiozsoy@hotmail.com>
2020-04-20 19:33:59 +0300

    init

---
+++ learn-swift-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learn-swift-ios
+//
+//  Created by Mac on 20.04.2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
